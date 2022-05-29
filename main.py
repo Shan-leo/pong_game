5d0c47f935601b9644f0182300a960e436e82d84
Shan-leo <bizwithshan@gmail.com>
2022-05-30 00:05:29 +0530

    Restarted project

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_len=5, stretch_wid=1)
+paddle.penup()
+paddle.goto(350,0)
 
 
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
 
-
-
-
-
+screen.listen()
+screen.onkey(go_up, "Up")
 
 screen.exitonclick()
 
