75e1b88c3badcefa33871d362a63a60402f02e2f
Shan-leo <bizwithshan@gmail.com>
2022-06-01 05:50:20 +0530

    Collision detection for up and down wall implemented

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen.setup(width=800, height=600)
 screen.title("Pong Game")
 screen.tracer(0)
 
-r_paddle = Paddle((350,0))
+r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 ball = Ball()
@@ -27,5 +27,10 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # detect collision
+    if ball.ycor() > 200 or ball.ycor() < -200:
+        ball.bounce()
+
 
 screen.exitonclick()
